12a1680f800193fac2f1d75cc923387265563e5b
Sean Corkery <scorkery@icloud.com>
2017-02-07 18:05:21 -0800

    added main method that prints some stuff

--- src/edu/pcc/cis/corkerya4/assignment4.java
+++ src/edu/pcc/cis/corkerya4/assignment4.java
@@ -4,4 +4,12 @@ package edu.pcc.cis.corkerya4;
  * Created by scorkery on 2/7/17.
  */
 public class assignment4 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Snibbidy dibbidy.");
+        System.out.println("I'm printing stuff to the console and you can't stop me.");
+
+    }
+
 }
